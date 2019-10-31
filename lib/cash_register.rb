82212b6191af86fc062a361a1f3ff7ad5c24be7b
Joseph Burks <joe_burks_3@yahoo.com>
2019-10-31 19:15:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
-
+class CashRegister
+  def initialize(discount=nil)
+    @discount = discount
+    @total = 0
+  end
+  
+  def discount
+    @discount
+  end
+  
+  def total
+    @total
+  end
+  
+  def total=(num)
+    @total = num
+  end
+  
+  def add_item(title, price, quantity=nil)
+    if quantity
+      @total += price * quantity
+    else
+      @total += price
+    end
+  end
+  
+  def apply_discount
+    if @discount
+      @total = @total - (@total * @discount / 100)
+      return "After the discount, the total comes to $#{@total}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+  
+end
